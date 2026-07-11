f16315c1a19bd23a8ee7e8bafcaa5f5cce4951d6
TomasLeTrain <tomylopezd@gmail.com>
2026-07-10 20:26:10 -0400

    improving printed labels

--- instructions/src/main.cpp
+++ instructions/src/main.cpp
@@ -15,15 +15,17 @@ int main() {
   // std::vector<OutputCategory> categories;
 }
 
-
 // TODO: define some state class to hold all the categories and actions
-void init(){
+void init() {
   std::vector<OutputCategory> categories;
+
+  // TODO: be able to look up category by name through state class
+  // const OutputCategory& bout_cat =
 
   OutputCategory bout_cat(4, 0, "bout");
   OutputCategory write_cat(4, 3, "write");
+  categories.push_back(bout_cat);
+  categories.push_back(write_cat);
 
-  // CategoryData data(&categories[0], 0);
-
-  Action pc_cnt("pc_cnt", {CategoryData(&bout_cat, 0)});
+  Action pc_cnt("pc_cnt", {{&bout_cat, 0}, {&write_cat, 0}});
 }
